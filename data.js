48974007d71af81aff9c565eed67fd2b19a83746
Dr-Opperman <51986608+Dr-Opperman@users.noreply.github.com>
2021-03-19 23:43:58 +0200

    InkJoint's Website

---
+++ data.js
@@ -0,0 +1,44 @@
+var song_list = {
+    "01":{
+        title: "ABAZALI",
+        artist: "Young Culure",
+        genre: "Hip Hop",
+        duration: "2:02",
+        track:"ABAZALI.mp3",
+    },
+    "02":{
+        title: "Dream Riding",
+        artist: "Young Culure",
+        genre: "Hip Hop",
+        duration: "4:08",
+        track:"dreaming ridding bendly coupe track.mp3",
+    },
+    "03":{
+        title: "Expression",
+        artist: "Young Culure",
+        genre: "Hip Hop",
+        duration: "3:46",
+        track:"expressions.mp3",
+    },
+    "04":{
+        title: "Walk the Talk",
+        artist: "Young Culure",
+        genre: "Hip Hop",
+        duration: "3:14",
+        track:"i wish a ma walk the talk track.mp3",
+    },
+    "05":{
+        title: "Your Best",
+        artist: "Young Culure",
+        genre: "Hip Hop",
+        duration: "2:29",
+        track:"put in your best 1.mp3",
+    },
+    "06":{
+        title: "Hunnid",
+        artist: "Hunnid Racks ft Kay-Mo",
+        genre: "Hip Hop",
+        duration: "3:05",
+        track:"Hunnid Racks ft Kay-Mo.mp3",
+    }
+}
